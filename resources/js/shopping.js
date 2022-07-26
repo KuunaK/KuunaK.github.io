6b5a7dd49fdc0db32be016e492265ab9f7e0ba61
KuunaK <KkalibK@Gmail.com>
2022-07-27 02:59:32 +1200

    add create list entry with functionality

--- resources/js/shopping.js
+++ resources/js/shopping.js
@@ -11,6 +11,8 @@ shoppingList.addEventListener("click", (e)=>{
     removeList(e);
     showActiveList(e);
     minimizeActiveList(e);
+    addListEntry(e);
+    deleteCheck(e);
 });
 
 
@@ -41,10 +43,52 @@ function addNewList(e){
         //Minimize button (hidden until active)
         const minimizeListButton = document.createElement("button");
         minimizeListButton.innerHTML = '<i class="fa-solid fa-circle-minus"></i>';
-        minimizeListButton.classList.add("minimize-list-button","not-active-button");
+        minimizeListButton.classList.add("minimize-list-button","no-display");
         newListDiv.appendChild(minimizeListButton);
 
+        // Create hidden list div
+        const listEntryDiv = document.createElement('div');
+        listEntryDiv.classList.add("list-item-entry-container", "no-display");
+        newListDiv.appendChild(listEntryDiv);
 
+        // Create form
+        const listEntryForm = document.createElement("form");
+        listEntryDiv.appendChild(listEntryForm);
+
+        // Create inner form div
+        const listEntryInnerDiv = document.createElement("div");
+        listEntryInnerDiv.classList.add("new-list-item-container");
+        listEntryForm.appendChild(listEntryInnerDiv);
+
+        // Create inner div input
+        const innerDivInput = document.createElement("input");
+        innerDivInput.classList.add("new-list-item-input");
+        innerDivInput.setAttribute("type", "text");
+        innerDivInput.setAttribute("id", "new-list-item-input");
+        innerDivInput.setAttribute("placeholder", "Add new entry");
+        innerDivInput.setAttribute("autocomplete", "off");
+        innerDivInput.setAttribute("maxlength", "20");
+        innerDivInput.setAttribute("required", "required");
+        listEntryInnerDiv.appendChild(innerDivInput);
+
+        // Add entry button
+        const innerDivButton = document.createElement("button");
+        innerDivButton.setAttribute("type", "submit");
+        innerDivButton.innerHTML = '<i class="fa-solid fa-plus"></i>';
+        innerDivButton.classList.add("new-entry-button");
+        listEntryInnerDiv.appendChild(innerDivButton);
+
+        //Add second outer div
+        const outerDiv = document.createElement("div");
+        outerDiv.classList.add("list-item-container");
+        listEntryDiv.appendChild(outerDiv);
+
+        //Add UL to new list items div
+        const outerDivUL = document.createElement("ul");
+        outerDivUL.classList.add("shopping-list-items");
+        outerDiv.appendChild(outerDivUL);
+
+        
         //Append to list
         shoppingList.appendChild(newListDiv);
 
@@ -75,12 +119,14 @@ function showActiveList (e) {
     const item = e.target;
     const minimizeListButton = item.querySelector(".minimize-list-button");
     const deleteListButton = item.querySelector(".delete-list-button");
+    const showHiddenList = item.querySelector(".list-item-entry-container");
     // Open list
     if (item.classList[0] === "shopping-list-div"){
         item.classList.remove("glowScale");
         item.classList.add("active-shopping-list");
-        minimizeListButton.classList.remove("not-active-button");
-        deleteListButton.classList.add("not-active-button");
+        minimizeListButton.classList.remove("no-display");
+        deleteListButton.classList.add("no-display");
+        showHiddenList.classList.remove("no-display");
     }
 };
 
@@ -88,11 +134,69 @@ function minimizeActiveList (e) {
     const item = e.target;
     const shoppingDiv = item.parentElement;
     const deleteListButton = item.parentElement.querySelector(".delete-list-button");
+    const showHiddenList = item.parentElement.querySelector(".list-item-entry-container");
     //Minize list
     if (item.classList[0] === 'minimize-list-button') {
         shoppingDiv.classList.remove("active-shopping-list");
         shoppingDiv.classList.add("glowScale");
-        item.classList.add("not-active-button");
-        deleteListButton.classList.remove("not-active-button");
+        item.classList.add("no-display");
+        deleteListButton.classList.remove("no-display");
+        showHiddenList.classList.add("no-display");
+    }
+};
+
+function addListEntry (e) {
+    e.preventDefault();
+    const item = e.target
+    const innerDivInput = item.parentElement.children[0]
+    
+    if ((item.classList[0] === "new-entry-button") && (innerDivInput.value.length > 0)) {
+        const shoppingListItems = item.parentElement.parentElement.parentElement.children[1].children[0]
+        //Create inner div
+        const newInnerListDiv = document.createElement("div");
+        newInnerListDiv.classList.add("inner-shopping-list-div");
+
+        // Create LI
+        const innerShoppingListEntry = document.createElement("li");
+        innerShoppingListEntry.innerText = innerDivInput.value;
+        innerShoppingListEntry.classList.add("inner-shopping-list-entry");
+        newInnerListDiv.appendChild(innerShoppingListEntry);
+
+        // Create Check button
+        const checkEntryButton = document.createElement("button");
+        checkEntryButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+        checkEntryButton.classList.add("check-entry-button");
+        newInnerListDiv.appendChild(checkEntryButton)
+
+        // Create Trash button
+        const trashEntryButton = document.createElement("button");
+        trashEntryButton.innerHTML = '<i class="fa-solid fa-minus"></i>';
+        trashEntryButton.classList.add("trash-entry-button");
+        newInnerListDiv.appendChild(trashEntryButton);
+
+        // Append to new list
+        shoppingListItems.appendChild(newInnerListDiv);
+
+        // Clear innerdiv input
+        innerDivInput.value = "";
+    }
+};
+
+function deleteCheck(e){
+    const item = e.target
+    console.log(item.parentElement)
+    //Delete entry
+    if (item.classList[0] === "trash-entry-button") {
+        const listEntry = item.parentElement;
+        // Animation
+        listEntry.classList.add("drop");
+        listEntry.addEventListener("transitionend", ()=>{
+            listEntry.remove();
+        });
+    }
+
+    if (item.classList[0] === "check-entry-button") {
+        const listEntry = item.parentElement;
+        listEntry.classList.toggle("completed")
     }
 };
